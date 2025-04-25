f413e193d007a3a19e1eed343c6681bf5b648596
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-04-25 22:17:46 +0300

    js

---
+++ js/classwork.js
@@ -0,0 +1,513 @@
+// function foo() {
+//     console.log(arguments);
+// };
+// foo(1, 2, 3);
+// foo(4, 5, 6, 7, 8, 9, 0);
+
+// function foo() {
+
+//     const arr = [1, 2, 3];
+//     console.log(Array.isArray(arr));
+//     console.log(Array.isArray(arguments));
+// };
+// foo(1, 2, 3);
+// foo(4, 5, 6, 7, 8, 9, 0);
+
+
+// function foo() {
+//     const args = Array.from(arguments);
+//     console.log(Array.isArray(args));
+// };
+// foo(1, 2, 3);
+// foo(4, 5, 6, 7, 8, 9, 0);
+
+// function foo() {
+//     console.log(arguments[1]);
+// };
+// foo(1, 2, 3);
+// foo(4, 5, 6, 7, 8, 9, 0);
+
+// function foo() {
+//     const args = Array.from(arguments);
+//     console.log(args);
+//     console.log(args.join(""));
+//     console.log(args.join(" "));
+// };
+// foo(1, 2, 3);
+// foo(4, 5, 6, 7, 8, 9, 0);
+
+// const add = function () {
+//     console.log(arguments);
+// };
+// console.log(add(1, 2, 3));
+// console.log(add(4, 5, 6, 7, 8, 9, 0));
+
+// const add = function () {
+//     const args = Array.from(arguments);
+//     let total = 0;
+//     for (let item of args) {
+//         total += item;
+//     }
+//     return total;
+// };
+// console.log(add(1, 2, 3));
+// console.log(add(4, 5, 6, 7, 8, 9, 0));
+
+// const calAverage = function () {
+//     let total = 0;
+//     for (let item of arguments) {
+//         total += item;
+//     }
+//     return total / arguments.length;
+// };
+// console.log(calAverage(1, 2, 3));
+// console.log(calAverage(4, 5, 6, 7, 8, 9, 0));
+
+
+// function logItems(items) {
+
+//     for (let i = 0; i < items.length; i++) {
+//         console.log(`${i+1} -${items[i]}`);
+//     }
+// }
+
+// logItems("njj", "fgh", "ghjj");
+// logItems(["njj", "fgh", "ghjj"]);
+
+
+
+
+// -------------------------------------------
+
+
+// function printInfo(names, phones) {
+//     const namesArr = names.split(",");
+//     console.log(namesArr);
+//     const phonesArr = phones.split(",");
+//     for (let i = 0; i < namesArr.length; i++){
+//         console.log(`${namesArr[i]}: ${phonesArr[i]}`);
+//     }
+// }
+
+// printInfo("Jacob, William, Anna, Maria", "12345, 4567, 7890, 89056")
+
+
+// ----------------------------------------------------------------------
+
+// function formatTime(totalMinutes) {
+//     const hours = Math.floor(totalMinutes / 60);
+//     const minutes = totalMinutes % 60;
+//     const doubleHours = String(hours).padStart(2, "0");
+//     const doubleMinutes = String(minutes).padStart(2, "0");
+//     return `${doubleHours}:${doubleMinutes}`;
+// }
+
+// console.log(formatTime(345));
+// console.log(formatTime(13));
+// console.log(formatTime(61));
+
+
+// -----------------------------------------------------------------------
+
+// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// function foo(arr, count) {
+//     const res = [];
+//     for (let i = 0; i < arr.length; i += count) {
+//         console.log(arr[i], i);
+//     }
+    
+// }
+// console.log(foo(data, 2));
+
+
+
+// -----------------------------------------------------------------------
+
+// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// function foo(arr, count) {
+//     const res = [];
+//     for (let i = 0; i < arr.length; i += count) {
+//         const comb = arr.slice(i, i + count);
+//         console.log(comb);
+//         res.push(comb);
+//     }
+//     return res;
+// }
+// console.log(foo(data, 3));
+
+
+// ------------------------------------------------------------------------
+
+// foo()
+// function foo() {
+//     console.log("lalala")
+// }
+
+// const foo = function () {
+//     console.log("tototo")
+// }
+// console.log(foo);
+// foo();
+
+// ------------------------------------------------------------------------
+
+
+// const fileName = "lalala.jpg";
+// function getName(str) {
+//     const res = str.split(".");
+//     return res;
+// }
+// console.log(getName(fileName));
+
+
+// -----------------------------------------------------------------------
+
+// const friends = ["Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"]
+
+// for (let i = 0; i < friends.length; i++){
+//     friends[i]=friends[i]+`-${[i+1]}`
+// };
+// console.log(friends);
+
+// friends[0] = "Petya + " + friends[0];
+// console.log(friends);
+
+// ------------------------------------------------------------------------
+
+// const values = "8 9";
+// const arr = values.split(" ");
+// const res = arr[0] * arr[1];
+// console.log(res);
+// const resulr = (arr[0] + arr[1])*2;
+// console.log(resulr);
+
+
+
+// -----------------------------------------------------------------------
+
+// const user = {
+//     name: "Alicie",
+//     age: "24",
+//     hobby: "html",
+//     premium: true,
+// }
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = "false";
+
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//     console.log(`${key}: ${user[key]}`);
+// }
+
+// --------------------------------------------------------------------------
+
+// const salaries = {
+//     Olya: 100,
+//     Petya: 120,
+//     Kolya: 110,
+// }
+// function sum(obj) {
+//     let total = 0;
+//     const values = Object.values(obj);
+//     console.log(values);
+//     for (const value of values) {
+//         total += value;
+//     }
+//     return total;
+// }
+// console.log(sum({}));
+// console.log(sum(salaries));
+
+
+// --------------------------------------------------------------------
+// function foo(storage, item) {
+//     const itemLower = item.toLowerCase();
+//     const isIncludes = storage.includes(itemLower);
+    
+//     if (isIncludes) {
+//         return item;
+//     } else {
+//         return "Sorry!";
+//     }
+// }
+// console.log(foo(["apple", "plum", "pear"], "Ar"));
+// console.log(foo(["apple", "plum", "pear"], "peAr"));
+// console.log(foo(["apple", "plum", "pear"], "orrAnge"));
+
+
+// 2.
+// function foo(storage, item) {
+//     const itemLower = item.toLowerCase();
+//     for (const item of storage) {
+//         if (item.includes(itemLower)) {
+//         return item;
+//         }
+//     }
+//     return "Sorry!";
+// }
+
+// console.log(foo(["apple", "plum", "pear"], "pe"));
+
+
+// --------------------------------------------------------------------
+
+// const friends = [
+//     { name: "Ross", online: false },
+//     { name: "Rachel", online: true },
+//     { name: "Monica", online: false },
+//     { name: "Phoebe", online: true },
+//     { name: "Joey", online: true },
+//     { name: "Chandler", online: false }
+// ]
+// // пошук друга за іменем
+
+// function findByName(arr, name) {
+//     for (const item of arr) {
+//         // console.log(item);
+//         // console.log(item.name);
+//         if (item.name.toLowerCase() === name.toLowerCase()) {
+//             return item.name;
+//         }
+//     }
+//     return "Not found!";
+// }
+// console.log(findByName(friends, "Rachel"));
+// console.log(findByName(friends, "Ra"));
+// console.log(findByName(friends, "RACHel"));
+
+
+// // імена всіх друзів
+// function getAllNames(arr){
+//     const namesArr = [];
+//     for (item of arr) {
+//         // console.log(item);
+//         namesArr.push(item.name);
+//     }
+//     return namesArr;
+// }
+// console.log(getAllNames(friends));
+
+// // імена дузів, які зараз онлайн
+// function getOnlineFriends(arr){
+//     const OnlineFriendsArr = [];
+//     for (item of arr) {
+//         // console.log(item);
+//         if(item.online) {
+//           OnlineFriendsArr.push(item.name);
+//         }
+//     }
+//     return OnlineFriendsArr;
+// }
+// console.log(getOnlineFriends(friends));
+
+
+
+// --------------------------------------------------------
+// const stones = [
+//     { name: "Смарагд", price: 800, quantity: 5 },
+//     { name: "Діамант", price: 1000, quantity: 10 },
+//     { name: "Сапфір", price: 500, quantity: 45 },
+//     { name: "Щебінь", price: 10, quantity: 100 },
+// ]
+
+// function totalPrice(arr, name) {
+//     let totalPrice = 0;
+//     for (const item of arr) {
+//         if (item.name === name) {
+//             totalPrice += item.price * item.quantity;
+//         }
+//     }
+//     return totalPrice;
+// }
+// console.log(totalPrice(stones, "Смарагд"));
+// console.log(totalPrice(stones, "Бірюза"));
+
+// -------------------------------------------------------------
+// const playList = {
+//     name: "My playlist",
+//     rating: 5,
+//     tracks: ["track - 1", "track - 2", "track - 3", "track - 4"],
+//     changeName(newName) {
+//         this.name = newName;
+//     },
+//     addTrack(newTrack) {
+//         this.tracks.push(newTrack);
+//     },
+//     updateRating(newRating) {
+//         this.rating = newRating;
+//     },
+//     getTrackcount() {
+//         return this.tracks.length;
+//     }
+// }
+
+// console.log(playList);
+
+// playList.changeName("New playlist");
+// playList.addTrack("track-5");
+// playList.updateRating(12);
+// playList.getTrackcount();
+
+// console.log(playList);
+// console.log(playList.name);
+// console.log(playList.tracks);
+// console.log(playList.rating);
+// console.log(playList.getTrackcount());
+
+
+// --------------------------------------------------------------------
+// const temperatures = [12, 35, 34, 23, 30, 18];
+
+// console.log(Math.max(temperatures));
+// console.log(Math.max(...temperatures));
+
+
+
+
+// -------------------------------------------------------------------
+
+// const objA = { x: 1, y: 2 };
+// const objB = { x: 3, z: 4 };
+
+// const objC = {
+//     ...objA,
+//     x:7,
+//     ...objB,
+//     y:9,
+// }
+
+// console.log(objC);
+
+// ----------------------------------------------------------------------
+// оновлюємо налаштування користувача
+// const dafaultSettings = {
+//     theme: "light",
+//     showNotifications: true,
+//     hideSidebar: false
+// };
+
+// const userSettings = {
+//     theme: "dark",
+//     showNotifications: false,
+//     hideSidebar: true
+// };
+
+// const finalSettings = {
+//     ...dafaultSettings,
+//    ...userSettings
+// };
+
+// console.log(finalSettings);
+
+
+
+// ---------------------------------------------------------
+// /*
+//  * Типів транзакцій всього два. Можна покласти чи зняти гроші з рахунку.
+//  */
+// const Transaction = {
+//   DEPOSIT: "deposit",
+//   WITHDRAW: "withdraw",
+// };
+  
+// const account = {
+//     // Поточний баланс рахунку
+//   balance: 0,
+  
+//     // Історія транзакцій
+//   transactions: [],
+  
+//     /*
+//      * Метод створює та повертає об'єкт транзакції.
+//        Кожна транзакція це об'єкт із властивостями: id, type та amount
+//      * Приймає суму та тип транзакції.
+//      */
+//   createTransaction(amount, type) {
+//     const obj = { id: amount, amount, type };
+//     return obj;
+//   },
+
+//     /*
+//      * Метод, що відповідає за додавання суми до балансу.
+//      * Приймає суму транзакції.
+//      * Викликає createTransaction для створення об'єкта транзакції
+//      * після чого додає його до історії транзакцій
+//      */
+//     deposit(amount) {
+//       this.balance += amount;
+//       const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+//       this.transactions.push(transaction);
+//     },
+  
+//     /*
+//      * Метод, що відповідає за зняття суми з балансу.
+//      * Приймає суму транзакції.
+//      * Викликає createTransaction для створення об'єкта транзакції
+//      * після чого додає його до історії транзакцій.
+//      *
+//      * Якщо amount більше ніж поточний баланс, виводь повідомлення
+//      * про те, що зняття такої суми не можливе, недостатньо коштів.
+//      */
+//     withdraw(amount) {
+//       if(this.balance < amount) {
+//         console.log("Недостатньо коштів");
+//         return;
+//       }
+
+//       this.balance -= amount;
+//       const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+//       this.transactions.push(transaction);
+//     },
+  
+//     /*
+//      * Метод повертає поточний баланс
+//      */
+//     getBalance() {
+//       return this.balance;
+//     },
+  
+//     /*
+//      * Метод шукає та повертає об'єкт транзакції по id
+//      */
+//     getTransactionDetails(id) {
+//       for(const item of this.transactions) {
+//         if(item.id === id) {
+//           return item;
+//         }
+//       }
+//     },
+  
+//     /*
+//      * Метод повертає кількість коштів
+//      * певного типу транзакції з усієї історії транзакцій
+//      */
+//     getTransactionTotal(type) {
+//       let sum = 0;
+//       for(const item of this.transactions) {
+//         if(item.type === type) {
+//           sum += item.amount;
+//         }
+//       }
+
+//       return sum;
+//     },
+// };
+
+
+// account.deposit(300);
+// account.deposit(100);
+
+// account.withdraw(200);
+
+// console.log(account.getBalance());
+// console.log(account.getTransactionDetails(300));
+// console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+// console.log(account.getTransactionTotal(Transaction.WITHDRAW));
+
+
+// console.log(account);
+
+
---
+++ js/lesson-2.js
@@ -0,0 +1,341 @@
+// ----------------------------------------------------------------------1.1
+// Створіть масив styles з елементами 'jazz' і 'blues'
+// Додайте до кінця масиву елемент 'rock-n-roll' за допомогою відповідного методу масивів
+// Знайдіть елемент 'blues' у масиві та замініть його на 'classic', використовуючи JavaScript-код
+
+// const styles = ["jazz", "blues"];
+// styles.push("rock-n-roll");
+// const blues = styles.indexOf("blues");
+// styles[blues] = "classic";
+// console.log(styles);
+
+// --------------------------------------------------------------------------1.2
+
+// Напишіть функцію logItems(array), яка приймає масив як аргумент
+// і виводить у консоль кожен його елемент у форматі:
+// "<номер елемента> - <значення елемента>".
+// Використайте цикл for для перебору елементів масиву.
+// Нумерація елементів повинна починатися з 1 (а не з 0).
+
+// function logItems(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(`${i + 1} - ${array[i]}`);
+//     }
+// }
+
+// logItems(["as", "ys", "kl", "yu"]);
+
+
+
+// -----------------------------------------------------------------------2
+// Напишіть функцію checkLogin(array), яка:
+// Приймає масив логінів як аргумент.
+// Запитує ім'я користувача через prompt.
+// Перевіряє, чи є введене ім'я у переданому масиві.
+// Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
+// Якщо ім'я відсутнє – виводить повідомлення: "User not found".
+
+// const logins = ["Peter", "John", "Igor", "Sasha"];
+ 
+// function checkLogin(array) {
+//        const name = prompt("What is your name?");
+//     if (array.includes(name)) {
+//        alert(`Welcome, ${name}!`);
+//     } else {
+//         alert("User not found");
+//     }
+      
+// }
+
+// checkLogin(logins);
+
+
+
+
+
+
+// -----------------------------------------------------------------------3
+// Напишіть функцію caclculateAverage(),
+// яка приймає довільну кількість
+// аргументів і повертає їхнє середнє значення.
+// Додайте перевірку, що аргументи - це числа.
+
+// function caclculateAverage() {
+//     const args = Array.from(arguments);     // перетворюємо псевдомасив arguments
+//                                             // (який містить усі передані в функцію значення)
+//                                             //  у справжній масив, з яким зручно працювати.
+//     const numbers = [];                     // Створюємо новий масив numbers,
+//                                             //  куди додамо тільки числа
+//     for (let i = 0; i < args.length; i++) {
+//         if (typeof args[i] === "number") {
+//             numbers.push(args[i]);
+//         }
+//     }
+
+//     if (numbers.length === 0) {
+//         return 0;
+//     }
+
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i];
+//     }
+
+//     return sum / numbers.length;
+// }
+
+// console.log(caclculateAverage(1, "fg", 24, 4, 9, "", 9));
+// console.log(caclculateAverage( "fg",  ""));
+
+
+
+// ----------------------------------------------------------------------4+
+// Напишіть функцію, яка сумуватиме сусідні числа
+// і пушитиме їх в новий масив.
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// уточнення: складати необхідно перше число з другим, потім друге - з третім,
+// третє - з четвертим і так до кінця.
+// В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
+// Adjacent — означає "сусідні".
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// function sumAdjacentPairs(arr) {
+//     const newArray = [];
+//     for (let i = 0; i < arr.length-1; i++) {
+//         sum = arr[i] + arr[i + 1];
+//         newArray.push(sum);
+//     }
+//     return newArray;
+// }
+
+// console.log(sumAdjacentPairs(someArr));
+
+
+// -----------------------------------------------------------------------5
+// Напишіть функцію findSmallestNumber(numbers),
+// яка шукає найменше число в масиві.
+// Додайте перевірку, що функція отримує саме масив, і
+// якщо функція отримує масив - поверніть з функції найменше число,
+// в іншому випадку - поверніть 'Sory, it is not an array!'.
+
+// const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
+
+// function findSmallestNumber(numbers) {
+//     if (Array.isArray(numbers)) {
+//       return  Math.min(...numbers);
+//     } else {
+//         return "Sorry, it is not an array!";
+//     }
+
+// }
+// console.log(findSmallestNumber(numbers));
+
+
+
+
+// -------------------------------------------------------------------6+
+// Напишіть функцію findLongestWord(string), яка
+// приймає довільний рядок, що складається лише зі слів, розділених
+// пробілами (параметр string), і повертатиме найдовше слово у реченні.
+
+// Скористайтесь цим прикладом виклику функції для перевірки її роботи:
+
+// function findLongestWord(string) {
+//     const arr = string.split(" ");
+//     const wordsLength = [];
+
+//     for (const item of arr) {
+//         wordsLength.push(item.length);
+//     }
+//     const longestWorld = Math.max(...wordsLength);
+//     return arr[wordsLength.indexOf(longestWorld)];
+// }
+
+
+// console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+
+// function findLongestWord(string) {
+//     const words = string.split(" ");
+//     let longest = words[0];
+
+//     for (let i = 1; i < words.length; i++) {
+//         if (words[i].length > longest.length) {
+//             longest = words[i];
+//         }
+//     }
+
+//     return longest;
+// }
+
+// console.log(findLongestWord("The quick brown fox jumps over the lazy dog"));
+
+// function findLongestWords(string) {
+//     const words = string.split(" ");
+//     let maxLength = 0;
+//     let longestWords = [];
+
+//     for (let i = 0; i < words.length; i++) {
+//         const wordLength = words[i].length;
+
+//         if (wordLength > maxLength) {
+//             maxLength = wordLength;
+//             longestWords = [words[i]]; // нове найдовше слово
+//         } else if (wordLength === maxLength) {
+//             longestWords.push(words[i]); // ще одне слово такої ж довжини
+//         }
+//     }
+
+//     return longestWords;
+// }
+// console.log(findLongestWords("I love watermelon and pineapples"));
+
+
+
+// ----------------------------------------------------------------------7
+// Напишіть скрипт, який для об'єкту user, послідовно:
+// 1 - додасть поле mood зі значенням 'happy',
+// 2 - замінить hobby на 'skydiving',
+// 3 - замінить значення premium на false,
+// 4 - виведе зміст об'єкта user у форматі
+// '<ключ>:<значення>' використовуя Object.keys() та for...of
+
+// const user = {
+//     name: "John",
+//     age: 20,
+//     hobby: "tenis",
+//     premium: true,
+//   };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const keys = Object.keys(user);
+
+// for (const key of keys) {
+//     console.log(`${key} : ${user[key]}`);
+// }
+
+
+
+// ----------------------------------------------------------------------8
+// Є об'єкт, в якому зберігаються зарплати команди
+// Напишіть код для додавання усіх зарплат та
+// збережіть його результат в змінній sum.
+// Якщо об'єкт salaries пустий, то результат має бути 0
+
+// const salaries = {
+//     Mango: 100,
+//     Poly: 160,
+//     Ajax: 1470,
+// };
+
+// const values = Object.values(salaries);
+
+// let sum = 0;
+
+// if (values.length === 0) {
+//     sum = 0;
+// } else {
+//     for (const value of values) {
+//     sum += value;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+
+
+// -------------------------------------------------------------------------9
+// Створіть об'єкт calculator з наступними методами:
+// read(a, b) - приймає два аргумента і зберігає їх як властивості об'єкта,
+// sum() - повертає сумму збереженних значень (з перевіркою на наявність властивостей в об'єкті),
+// mult() - перемножає збереженні значення і повертає результат (з перевіркою на наявність властивостей в об'єкті),
+// винесіть перевірку на наявність властивостей в об'єкті в окремий метод exist().
+
+// Якщо вказані властивості в обʼєкті відсутні (тобто метод exist повертає false),
+// методи sum і mult мають повертати рядок 'No such propeties'
+
+// const calculator = {
+//     x: undefined,
+//     y: undefined,
+
+//     read(a, b) {
+//         this.x = a;
+//         this.y = b;
+//     },
+
+//     exist() {
+//         return this.x !== undefined && this.y !== undefined;
+//     },
+
+//     sum(a, b) {
+//         if (!this.exist()) {
+//             return "No such properties";
+//         }
+//         return this.x + this.y;
+//     },
+//     mult() {
+//        if (!this.exist()) {
+//             return "No such properties";
+//         }
+//         return this.x * this.y;
+//     },
+  
+// }
+
+// calculator.read(4, 9);
+// console.log(calculator.sum());
+// console.log(calculator.mult());
+
+
+
+// --------------------------------------------------------------------------10
+// Напишіть функцію calcTotalPrice(fruits, fruitName),
+// яка приймає масив об'єктів (fruits) і рядок з назвою фрукта (fruitName).
+// Функція рахує і повертає загальну вартість фрукта
+// з таким ім'ям, ціною та кількістю з об'єкта.
+
+// Зверніть увагу, що в масиві може бути кілька обʼєктів з однаковою 
+// назвою фрукта, це також треба урахувати.
+
+const fruits = [
+    { name: "Яблуко", price: 45, quantity: 7 },
+    { name: "Апельсин", price: 60, quantity: 4 },
+    { name: "Банан", price: 125, quantity: 8 },
+    { name: "Груша", price: 350, quantity: 2 },
+    { name: "Виноград", price: 440, quantity: 3 },
+    { name: "Банан", price: 125, quantity: 3 },
+  ];
+
+function calcTotalPrice(fruits, fruitName) {
+    let totalPrice = 0;
+    let totalQuantity = 0;
+    for (const item of fruits) {
+        if (item.name === fruitName) {
+            totalQuantity +=item.quantity;
+            totalPrice += item.price * item.quantity;;
+        } 
+               
+    }
+
+      if (totalQuantity === 0) {
+        return `Фрукта "${fruitName}" не знайдено на складі.`;
+    }
+
+    return `На складі ${totalQuantity} кілограм(и) ${fruitName},
+    загальна вартість яких складає ${totalPrice} гривень`
+}
+  
+console.log(calcTotalPrice(fruits, "Банан"));
+console.log(calcTotalPrice(fruits, "Апельсин"));
+console.log(calcTotalPrice(fruits, "Абрикос"));
+
+
+
